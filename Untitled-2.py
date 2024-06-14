6f56312d5236b9fdd1ecaa588137f07f68c433ab
Ali $@ <90723218+Alisaberi1@users.noreply.github.com>
2024-06-14 11:25:29 +0330

    Add files via upload

---
+++ Untitled-2.py
@@ -0,0 +1,16 @@
+
+
+Command_from_users=""
+while Command_from_users=="quit":
+    Command_from_users=input(":").lower()
+    if Command_from_users=="start":print("Car started")
+    if Command_from_users=="stop":print("Car stoped")
+    if Command_from_users=="help":print("""
+    start_to start the car
+    stop_ to stop the car
+    quit_ to quit
+    """)
+    elif Command_from_users=="quit":break 
+    else:
+        print("I don't understand that!")
+        
