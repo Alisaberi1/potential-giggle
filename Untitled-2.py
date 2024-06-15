c6cd3a55bb4595bc273067ade81e7fa46fb54e4c
Ali $@ <90723218+Alisaberi1@users.noreply.github.com>
2024-06-15 22:22:39 +0330

    Update Untitled-2.py

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,16 +1,19 @@
 
 
 Command_from_users=""
-while Command_from_users=="quit":
+while True:
     Command_from_users=input(":").lower()
-    if Command_from_users=="start":print("Car started")
-    if Command_from_users=="stop":print("Car stoped")
-    if Command_from_users=="help":print("""
+    if Command_from_users=="start":
+        print("Car started")
+    if Command_from_users=="stop":
+        print("Car stoped")
+    if Command_from_users=="help":
+        print("""
     start_to start the car
     stop_ to stop the car
-    quit_ to quit
-    """)
-    elif Command_from_users=="quit":break 
+    quit_ to quit""" )
+    if Command_from_users=="quit":
+        break 
     else:
         print("I don't understand that!")
         
